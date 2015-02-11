6bb6c343547eb33734d6dbbf7c5bb52607a233cc
Todd Hiestand <todd@purecharity.com>
2015-02-11 13:51:27 -0800

    Add leadership formatting v.1.2.0

--- page-leadership.php
+++ page-leadership.php
@@ -10,7 +10,7 @@ Template Name: Leadership Template
 	<div class="wide-container">
 		<div class="container">
 			<div class="row">
-				<div class="page-content leadership-page col span_24">
+				<div class="page-content col span_24">
 					<?php while ( have_posts() ) : the_post(); ?>
 					<?php the_content(); ?>
 				</div>
@@ -19,25 +19,25 @@ Template Name: Leadership Template
 	</div>
 
 	<?php if( have_rows('leadership') ): ?>
-		<div id="leader-section">
+		<div class="leader-section">
 		<?php while( have_rows('leadership') ): the_row(); ?>
 			<div class="wide-container">
 				<div class="container">
-					<h3><?php the_sub_field('group_title'); ?></h3>
+				<h2><?php the_sub_field('group_title'); ?></h2>
 						<?php if( have_rows('leadership_group') ): ?>
 							<div class="row">
-								<div class="leader-container col span_24">
+								<ul class="leader-container col span_24">
 								<?php while( have_rows('leadership_group') ): the_row(); ?>
-									<div class="col span_8">
+									<li class="leader col span_8">
 										<h3><?php the_sub_field('name'); ?></h3>
 										<img class="rounded-avatar" src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('name'); ?>">
 										<p><?php the_sub_field('position'); ?></p>
 										<?php while( have_rows('connections') ): the_row();?>
 											<a class="ss-icon ss-social-circle" href="<?php the_sub_field('link'); ?>"><?php the_sub_field('link_name'); ?></a>
 										<?php endwhile; ?>
-									</div>
+									</li>
 								<?php endwhile; ?>
-								</div>
+								</ul>
 							</div>
 						<?php endif; //if( get_sub_field('items') ): ?>
 				</div>
@@ -46,25 +46,25 @@ Template Name: Leadership Template
 	<?php endif; // if( get_field('to-do_lists') ): ?>
 
 	<?php if( have_rows('secondary_leadership_section') ): ?>
-		<div id="leader-section">
+		<div class="leader-section secondary-leaders">
 		<?php while( have_rows('secondary_leadership_section') ): the_row(); ?>
 			<div class="wide-container">
 				<div class="container">
-					<h3><?php the_sub_field('group_title'); ?></h3>
+					<h2><?php the_sub_field('group_title'); ?></h2>
 						<?php if( have_rows('leadership_group') ): ?>
 							<div class="row">
-								<div class="leader-container col span_24">
+								<ul class="leader-container col span_24">
 								<?php while( have_rows('leadership_group') ): the_row(); ?>
-									<div class="col span_4">
+									<li class="leader secondary-leader col span_4">
 										<h4><?php the_sub_field('name'); ?></h4>
 										<img class="rounded-avatar" src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('name'); ?>">
 										<p><?php the_sub_field('position'); ?></p>
 										<?php while( have_rows('connections') ): the_row();?>
 											<a class="ss-icon ss-social-circle" href="<?php the_sub_field('link'); ?>"><?php the_sub_field('link_name'); ?></a>
 										<?php endwhile; ?>
-									</div>
+									</li>
 								<?php endwhile; ?>
-								</div>
+								</ul>
 							</div>
 						<?php endif; //if( get_sub_field('items') ): ?>
 				</div>
