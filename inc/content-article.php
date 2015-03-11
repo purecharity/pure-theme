0390461e9da68fe10e97ffdcc347ebfb81754404
Todd Hiestand <todd@purecharity.com>
2015-03-11 12:48:33 -0700

    Use square image in blog listing

--- inc/content-article.php
+++ inc/content-article.php
@@ -1,6 +1,8 @@
 <p class="meta date"><span class="ss-icon ss-pika">date</span><?php the_time('F j, Y'); ?><span class="author"><span class="ss-icon ss-pika">user</span>Posted by: <?php the_author_posts_link(); ?></span></p>
 <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-<?php the_post_thumbnail( 'header' ); ?>    
+<?php the_post_thumbnail( 'image-square' ); ?>    
 <p><?php the_excerpt();?></p>
+<div class="col span_24">
 <?php the_tags('<span class="meta tags"><span class="tags-title"><span class="ss-icon ss-pika">tag</span>Tags:</span> ', ', ', '</p>'); ?>
 <p class="meta categories"><span class="ss-icon ss-pika">pushpin</span>Categories: <?php the_category(',') ?></p>
+</div>
