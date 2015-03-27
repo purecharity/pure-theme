bd9c071c8ebd6b6d2c2c851d6fa2b793bdb7e0e1
Todd Hiestand <todd@purecharity.com>
2015-03-27 12:07:24 -0700

    Add blog header and overlay option

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 <div class="wide-container page-head">
   <?get_template_part('inc/nav','dark');?>
-  <div class="wide-container page-header overlayon" style="color:<?php the_field('page_header_text_color'); ?>;background-image: url(<?php if( get_post_meta($post->ID, "page_header", true) ): ?><?php the_field('page_header'); ?><?php else: ?><?php the_field('default_page_header', 'option'); ?><?php endif; ?>);">
+  <div class="wide-container page-header <?php the_field('blog_overlay', 'option'); ?>" style="color:<?php the_field('page_header_text_color'); ?>;background-image: url(<?php if( get_post_meta($post->ID, "page_header", true) ): ?><?php the_field('page_header'); ?><?php else: ?><?php the_field('blog_landing_header', 'option'); ?><?php endif; ?>);">
     <div class="row nopadrow">
       <div class="col span_24 centered page-header-content">
           <h1>Blog</h1>
