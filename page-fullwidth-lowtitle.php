12bb19933ec32a0a0c57526c6c161a65080b29c8
Todd Hiestand <HotRod@Todds-MacBook-Pro.local>
2015-09-02 14:38:18 -0700

    Add leadership modals

--- page-fullwidth-lowtitle.php
+++ page-fullwidth-lowtitle.php
@@ -7,9 +7,9 @@ Template Name: Full Width Page Template - Title in Body
 	get_template_part( 'inc/pageheader','image-bodytitle' ); 
 ?>
 
-<div class="wide-container">
+<div class="wide-container page-title">
 	<div class="container">
-	<div class="row centered">
+	<div class="row centered nopadbottom">
 		<h1><?php the_title();?></h1>
     <?php if( $head_subtext = get_field('header_subtext') ){ ?>
       <?php echo $head_subtext; ?>
--- page-leadership.php
+++ page-leadership.php
@@ -7,6 +7,9 @@ Template Name: Leadership Template
 	get_template_part( 'inc/pageheader','image'); 
 ?>
 
+	<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/simplemodal.js"></script>
+
+
 	<div class="wide-container">
 		<div class="container">
 			<div class="row">
@@ -25,17 +28,38 @@ Template Name: Leadership Template
 				<div class="container">
 				<h2><?php the_sub_field('group_title'); ?></h2>
 						<?php if( have_rows('leadership_group') ): ?>
+							<?php $counter = 1;  //this sets up the counter starting at 0 ?>
 							<div class="row">
 								<ul class="leader-container col span_24">
 								<?php while( have_rows('leadership_group') ): the_row(); ?>
 									<li class="leader col span_8">
-										<h3><?php the_sub_field('name'); ?></h3>
-										<img class="rounded-avatar" src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('name'); ?>">
-										<p><?php the_sub_field('position'); ?></p>
-										<?php while( have_rows('connections') ): the_row();?>
-                      <a class="leadercontact ss-icon ss-social-circle" href="<?php the_sub_field('link'); ?>"><?php the_sub_field('link_name'); ?></a>
-										<?php endwhile; ?>
+									<?php if( $profile = get_sub_field('profile') ){ ?>
+			               <a data-leader-id="lead1-<?php echo $counter; ?>" class="modal-link" href="#">
+			             <?php }?>
+											<h3><?php the_sub_field('name'); ?></h3>
+											<img class="rounded-avatar" src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('name'); ?>">
+											<p><?php the_sub_field('position'); ?></p>
+											<?php while( have_rows('connections') ): the_row();?>
+                    	  <a class="leadercontact ss-icon ss-social-circle" href="<?php the_sub_field('link'); ?>"><?php the_sub_field('link_name'); ?></a>
+											<?php endwhile; ?>
+											<?php if( $profile = get_sub_field('profile') ){ ?>
+			               	</a>
+			            	 <?php }?>
+	            	 <!-- add the modal window within the loop -->
+	            	<div id="basic-modal-content" class="modal" data-leader-id="lead1-<?php echo $counter; ?>">
+									<div class="row nopadrow">
+										<div class="col span_6">
+											<img class="rounded-avatar" src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('name'); ?>">
+										</div>
+										<div class="col span_18">
+											<h3><?php the_sub_field('name'); ?></h3>
+											<?php the_sub_field('profile'); ?>
+										</div>
+								</div>
+								<!-- end modal window -->
+									<?php $counter++; // add one per row ?> 
 									</li>
+
 								<?php endwhile; ?>
 								</ul>
 							</div>
@@ -52,16 +76,35 @@ Template Name: Leadership Template
 				<div class="container">
 					<h2><?php the_sub_field('group_title'); ?></h2>
 						<?php if( have_rows('leadership_group') ): ?>
+							<?php $counter = 1;  //this sets up the counter starting at 0 ?>
 							<div class="row">
 								<ul class="leader-container col span_24">
 								<?php while( have_rows('leadership_group') ): the_row(); ?>
 									<li class="leader secondary-leader col span_6">
+										<?php if( $profile = get_sub_field('profile') ){ ?>
+			               <a data-leader-id="lead2-<?php echo $counter; ?>" class="modal-link" href="#">
+			             <?php }?>
 										<h4><?php the_sub_field('name'); ?></h4>
 										<img class="rounded-avatar" src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('name'); ?>">
 										<p class="leader-position"><?php the_sub_field('position'); ?></p>
 										<?php while( have_rows('connections') ): the_row();?>
                       <a class="leadercontact ss-icon ss-social-circle" href="<?php the_sub_field('link'); ?>"><?php the_sub_field('link_name'); ?></a>
 										<?php endwhile; ?>
+										<?php if( $profile = get_sub_field('profile') ){ ?>
+			               	</a>
+			            	 <?php }?>
+			            	  <!-- add the modal window within the loop -->
+				            	<div id="basic-modal-content" class="modal" data-leader-id="lead2-<?php echo $counter; ?>">
+												<div class="row nopadrow">
+													<div class="col span_6">
+														<img class="rounded-avatar" src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('name'); ?>">
+													</div>
+													<div class="col span_18">
+														<h3><?php the_sub_field('name'); ?></h3>
+														<?php the_sub_field('profile'); ?>
+													</div>
+											</div>
+											<!-- end modal window -->
 									</li>
 								<?php endwhile; ?>
 								</ul>
@@ -82,3 +125,23 @@ Template Name: Leadership Template
 	</div> 
 
 <?php get_footer(); ?>
+
+
+
+<script>
+
+jQuery(function ($) {
+	// Load dialog on page load
+	//$('#basic-modal-content').modal();
+
+	// Load dialog on click
+	$('.modal-link').click(function (e) {
+		$('.modal[data-leader-id='+ $(this).attr('data-leader-id') +']').modal();
+
+		return false;
+	});
+});
+
+</script>
+
+
